f3f991c2db90cfca539c3c1381e84b5922fa47c3
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-22 22:14:48 +0530

    demo commit

---
+++ src/main/java/com/example/Wallcart/WallcartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wallcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Wallcart/WallcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wallcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
